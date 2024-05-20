52fd4120920752bfad30e4a2d052f018abdf4131
Qifaysibr <afim121@gmail.com>
2024-05-20 20:59:09 +0700

    projek laravel

---
+++ pertemuan6/pengenalanDiri/resources/views/perkenalanDiri.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syafiq Ibrahim</title>
+
+    
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+   
+</head>
+<body>
+    <header class="navbar-container">
+        <div class="logo">
+            <h1>Syafiq Ibrahim</h1>
+        </div>
+        <nav class="nav-list">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#contact ">Contact us</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+      <aside>
+          <section id="contact">
+            <aside>
+              <div class="container">
+                <div class="content-image">
+                  <img src="{{asset('img/syafiq.jpg')}}" alt="syafiq">
+                </div>
+                <div class="content-description">
+                  <h4>Software Developer</h4>
+                  <h1>Syafiq Ibrahim</h1>
+                  <h6>Mahasiswa Teknik Informatika</h6>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora sit corporis cum dignissimos labore esse molestiae consectetur, totam nostrum, sapiente corrupti maiores dolorum maxime excepturi vero. Odio nobis suscipit earum!<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, ut beatae repudiandae perspiciatis ratione minus incidunt ab saepe temporibus adipisci nemo, nihil aperiam cupiditate omnis exercitationem numquam porro fugit aliquid?</p>
+                  <div class ="frame">
+                    
+                    <a href="https://twitter.com/Qifays51626433" class="btn">
+                  <i class="fab fa-twitter" style="color: #00acee;"></i>
+                </a>
+                    <a href="https://www.instagram.com/_afim_afim_/" class="btn">
+                 <i class="fab fa-instagram" style="color: #ea4c89;"></i>
+                </a>
+                    <a href="https://www.linkedin.com/in/syafiq-ibrahim-7a8a89276/" class="btn">
+                 <i class="fab fa-linkedin-in" style="color:#0e76a8;"></i>
+                </a>
+                    <a href="https://github.com/Qifaysibr/" class="btn">
+                 <i class="fab fa-github" ></i>
+                </a>
+                  </div>
+                </div>
+              </div>
+          </aside>
+          </section>
+      </aside>
+       </main>
+       <footer>
+        <p>Syafiq ibrahim &#169; 2024</p>
+      </footer>
+       
+</body>
+</html>
